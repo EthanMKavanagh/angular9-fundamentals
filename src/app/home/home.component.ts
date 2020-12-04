bdda379ee9b49d5a828822a4bd69b4d10cd5ea57
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-12-04 11:26:11 -0600

    moved lessons into service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LessonsService } from '../shared/services/lessons.service';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +10,16 @@ export class HomeComponent implements OnInit {
   title = 'Hello World';
   currentLesson = null;
   
-  courseLessons = [
-    { title: 'Hello Angular' },
-    { title: 'Component Fundamentals' },
-    { title: 'Template Driven Forms' },
-    { title: 'Angular Services' },
-    { title: 'Server Communication' },
-    { title: 'Component Driven Architecture' },
-    { title: 'Angular Routing' },
-    { title: 'Unit Testing Fundamentals' },
-  ];
+  courseLessons = null;
 
-  constructor() { }
+  constructor(private lessonsService: LessonsService) { }
 
   ngOnInit(): void {
+    this.courseLessons = this.lessonsService.all();
   }
 
   selectLesson(lesson) {
+    console.log('SELECTED LESSON FIRED!', lesson);
     this.currentLesson = lesson;
   }
 }
