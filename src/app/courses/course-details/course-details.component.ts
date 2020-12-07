8a07c24849b0f2f6357c381f73da72ddd480b1fe
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-12-07 15:53:18 -0600

    getting rid of shared mutable state

--- src/app/courses/course-details/course-details.component.ts
+++ src/app/courses/course-details/course-details.component.ts
@@ -1,18 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.scss']
 })
-export class CourseDetailsComponent implements OnInit {
-  @Input() course;
+export class CourseDetailsComponent {
+  selectedCourse;
+  originalTitle;
+
   @Output() saved = new EventEmitter();
   @Output() cancelled = new EventEmitter();
   
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set course(value) {
+    if (value) {
+      this.selectedCourse = Object.assign({}, value);
+      this.originalTitle = value.title;
+    }
   }
-
 }
